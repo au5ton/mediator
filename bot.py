5729bf864c15d589e8268a03b903af53399cc381
Austin Jackson <austinjckson@gmail.com>
2019-03-28 18:40:53 -0500

    starting

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 from dotenv import load_dotenv, find_dotenv
 from pyrogram import Client, Filters, MessageHandler
 import os
+import optparse
 load_dotenv(find_dotenv())
 
 PROJECT_DIRECTORY = os.path.split(os.path.realpath(__file__))[0]
@@ -16,5 +17,12 @@ app = Client(
 @app.on_message(Filters.command(["start", "help"]))
 def start_command(client, message):
     print(message)
+
+@app.on_message(Filters.command(["newchat"]))
+def new_chat(client, message):
+    print(message)
+    if message.text != None and message.text.startswith("/newchat"):
+        message.reply(f'Chat name: \'{message.text.lstrip("/newchat").strip()}\'', quote=True)
+        
 
 app.run()
